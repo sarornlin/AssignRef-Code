b8dcf5ba3bb05ba36d9bff86a177e7b60610707d
Sarorn Lin <127047880+sarornlin@users.noreply.github.com>
2023-07-21 18:13:43 -0400

    Update IStripeOrderReceiptService.cs

--- DotNET/IStripeOrderReceiptService.cs
+++ DotNET/IStripeOrderReceiptService.cs
@@ -1,9 +1,4 @@
-﻿using Sabio.Models.Requests.Stripe;
-
-namespace Sabio.Services.Interfaces
-{
     public interface IStripeOrderReceiptService
     {
         int Create(OrderReceiptAddRequest model, int userId);
     }
-}
