ee149afb14561ab7115f895a87f8b22785b7347e
Sarorn Lin <127047880+sarornlin@users.noreply.github.com>
2023-07-21 18:13:50 -0400

    Update IStripeTransferService.cs

--- DotNET/IStripeTransferService.cs
+++ DotNET/IStripeTransferService.cs
@@ -1,10 +1,4 @@
-﻿using Sabio.Models.Requests.Stripe;
-using Stripe;
-
-namespace Sabio.Services.Interfaces
-{
     public interface IStripeTransferService
     {
         int CreateTransfer(TransferAddRequest model, int userId);
     }
-}
